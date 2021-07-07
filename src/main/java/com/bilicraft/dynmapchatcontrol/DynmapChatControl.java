9c38258fc1086fb5e91779320ca45f87f9556bc5
Ghost_chu <2908803755@qq.com>
2021-07-07 11:47:21 +0800

    Initial commit

---
+++ src/main/java/com/bilicraft/dynmapchatcontrol/DynmapChatControl.java
@@ -0,0 +1,60 @@
+package com.bilicraft.dynmapchatcontrol;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.dynmap.DynmapAPI;
+import org.dynmap.DynmapWebChatEvent;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class DynmapChatControl extends JavaPlugin implements Listener {
+    private DynmapAPI api;
+    private final Set<String> blockedSource = new HashSet<>();
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this,this);
+        api = (DynmapAPI) Bukkit.getPluginManager().getPlugin("dynmap");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void dynmapChat(DynmapWebChatEvent event){
+        if(blockedSource.contains(event.getName())){
+            event.setCancelled(true);
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!sender.hasPermission("dwebchat.admin")){
+            return false;
+        }
+        if(args.length != 2){
+            sender.sendMessage("Usage: /dwebchat <mute/unmute> <source>");
+            return true;
+        }
+        switch (args[0]){
+            case "mute":
+                this.blockedSource.add(args[1]);
+                sender.sendMessage("操作成功，禁言对象将持续到下次服务器重启");
+                break;
+            case "unmute":
+                this.blockedSource.remove(args[1]);
+                sender.sendMessage("操作成功");
+                break;
+        }
+       return true;
+
+    }
+}
